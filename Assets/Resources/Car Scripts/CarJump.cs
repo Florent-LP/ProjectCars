f09dbf6ccc59e809923e825e5a9e2943e9c0c64b
FidelNguyen <fidelnguyen@gmail.com>
2016-06-03 18:06:13 +0200

    Version Présentation

--- Assets/Resources/Car Scripts/CarJump.cs
+++ Assets/Resources/Car Scripts/CarJump.cs
@@ -7,6 +7,11 @@ public class CarJump : MonoBehaviour {
 	public float jumpSpeed = 100f;
 	public KeyCode jumpControl;
 
+	public double JumpRate = 0.5;
+	private double nextJump = 0.0;
+
+
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent <Rigidbody>();
@@ -14,7 +19,8 @@ public class CarJump : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (jumpControl)){
+		if (Input.GetKeyDown (jumpControl) && Time.time > nextJump){
+			nextJump = Time.time + JumpRate;
 			rb.velocity += jumpSpeed * Vector3.up;
 		} 
 	}
---
+++ Assets/Resources/Car Scripts/respawnManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class respawnManager : MonoBehaviour {
+	protected Vector3 initialPos;
+	protected Quaternion initialRot;
+	protected Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = gameObject.transform.position;
+		initialRot = gameObject.transform.rotation;
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Terrain") {
+			gameObject.transform.position = initialPos;
+			gameObject.transform.rotation = initialRot;
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+		}
+	}
+}
