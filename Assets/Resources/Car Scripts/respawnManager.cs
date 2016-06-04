6ed28e95831f04b5d9cfb3200d8069ef6f0b5c11
Florent-LP <lp.florent@gmail.com>
2016-06-04 10:39:02 -0400

    Checkpoints

--- Assets/Resources/Car Scripts/respawnManager.cs
+++ Assets/Resources/Car Scripts/respawnManager.cs
@@ -5,11 +5,15 @@ public class respawnManager : MonoBehaviour {
 	protected Vector3 initialPos;
 	protected Quaternion initialRot;
 	protected Rigidbody rb;
+	protected Vector3 lastCheckPos;
+	protected Quaternion lastCheckRot;
 
 	// Use this for initialization
 	void Start () {
 		initialPos = gameObject.transform.position;
 		initialRot = gameObject.transform.rotation;
+		lastCheckPos = initialPos;
+		lastCheckRot = initialRot;
 		rb = GetComponent<Rigidbody> ();
 	}
 	
@@ -20,10 +24,14 @@ public class respawnManager : MonoBehaviour {
 
 	void OnTriggerEnter(Collider c) {
 		if (c.gameObject.tag == "Respawn") {
-			gameObject.transform.position = initialPos;
-			gameObject.transform.rotation = initialRot;
+			gameObject.transform.position = lastCheckPos;
+			gameObject.transform.rotation = lastCheckRot;
 			rb.velocity = Vector3.zero;
 			rb.angularVelocity = Vector3.zero;
+		} else if (c.gameObject.tag == "CheckPoint") {
+			lastCheckPos = c.gameObject.transform.position;
+			lastCheckRot = c.gameObject.transform.rotation;
 		}
+			
 	}
 }
